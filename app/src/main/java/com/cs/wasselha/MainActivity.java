22da2856b6bd89eef86bf7c6c02f03a483445cb0
ghassan-amreyah <1191704@student.birzeit.edu>
2023-04-28 00:56:13 +0300

    Start build UI

--- app/src/main/java/com/cs/wasselha/MainActivity.java
+++ app/src/main/java/com/cs/wasselha/MainActivity.java
@@ -2,13 +2,55 @@ package com.cs.wasselha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        //References
+        setupReference();
+        loginSetup();
+        signupSetup();
     }
+
+
+    //References
+    private void setupReference() {
+        loginBtn = findViewById(R.id.loginBtnInRegisterPage);
+        signupBtn = findViewById(R.id.signupBtnInRegisterPage);
+    }
+
+    private void loginSetup() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void signupSetup() {
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, TypeSignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/cs/wasselha/TypeSignupActivity.java
@@ -0,0 +1,15 @@
+package com.cs.wasselha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TypeSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_signup);
+        getSupportActionBar().hide();
+    }
+}
