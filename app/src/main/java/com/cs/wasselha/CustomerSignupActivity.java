c97d30c37691451a4b1358abf68bb365eea4112a
Aws Ayyash <aws.ayyash.1@gmail.com>
2023-04-29 01:41:03 +0300

    adding customer signup activity

---
+++ app/src/main/java/com/cs/wasselha/CustomerSignupActivity.java
@@ -0,0 +1,14 @@
+package com.cs.wasselha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_signup);
+    }
+}
