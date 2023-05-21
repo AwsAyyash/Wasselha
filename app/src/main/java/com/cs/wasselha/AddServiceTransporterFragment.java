223f9844441ec0d1232054dfa67de9c1d556ca04
ghassan-amreyah <1191704@student.birzeit.edu>
2023-05-22 00:14:18 +0300

    Continue building of transporter fragments

--- app/src/main/java/com/cs/wasselha/AddServiceTransporterFragment.java
+++ app/src/main/java/com/cs/wasselha/AddServiceTransporterFragment.java
@@ -1,5 +1,6 @@
 package com.cs.wasselha;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddServiceTransporterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddServiceTransporterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    NumberPicker hoursPicker, minutesPicker, amPmPicker;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddServiceTransporterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddServiceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddServiceTransporterFragment newInstance(String param1, String param2) {
-        AddServiceTransporterFragment fragment = new AddServiceTransporterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_service_transporter, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_service_transporter, container, false);
+
+
+        //References
+        setUpReferences(view);
+
+        return view;
     }
+
+
+
+    //References
+    private void setUpReferences(View view)
+    {
+        hoursPicker = view.findViewById(R.id.numPickerHourInAddNewService);
+        hoursPicker.setTextColor(Color.WHITE);
+        hoursPicker.setMinValue(1);
+        hoursPicker.setMaxValue(12);
+
+        minutesPicker = view.findViewById(R.id.numPickerMinInAddNewService);
+        minutesPicker.setTextColor(Color.WHITE);
+        minutesPicker.setMinValue(0);
+        minutesPicker.setMaxValue(59);
+
+        amPmPicker = view.findViewById(R.id.numPickerAMPMInAddNewService);
+        amPmPicker.setTextColor(Color.WHITE);
+        amPmPicker.setMinValue(0);
+        amPmPicker.setMaxValue(1);
+        amPmPicker.setDisplayedValues(new String[] {"AM", "PM"});
+
+    }
+
+
 }
--- app/src/main/java/com/cs/wasselha/HomeTransporterFragment.java
+++ app/src/main/java/com/cs/wasselha/HomeTransporterFragment.java
@@ -1,64 +1,45 @@
 package com.cs.wasselha;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeTransporterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class HomeTransporterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeTransporterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeTransporterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeTransporterFragment newInstance(String param1, String param2) {
-        HomeTransporterFragment fragment = new HomeTransporterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    ListView listView;
+    private ArrayList<Services> servicesData;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_transporter, container, false);
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_home_transporter, container, false);
+        listView = view.findViewById(R.id.listViewInMainPageTransporter);
+
+        populateServicesData();
+
+        ServicesAdapter servicesAdapter = new ServicesAdapter(requireContext(), R.layout.home_page_transporter_list_view, servicesData);
+        listView.setAdapter(servicesAdapter);
+
+        return view;
+    }
+
+    private void populateServicesData()
+    {
+        servicesData = new ArrayList<>();
+        servicesData.add(new Services(R.drawable.car1, "Ramallah", "Nablus", "9:00 AM"));
+        servicesData.add(new Services(R.drawable.car1, "Nablus", "Jenin", "9:30 AM"));
+        servicesData.add(new Services(R.drawable.car1, "Birzeit", "Ramallah", "10:00 AM"));
+        servicesData.add(new Services(R.drawable.car1, "Jenin", "Nablus", "10:30 AM"));
+
     }
 }
