a37dc096ac1e1fc7746a785680244c82cd636767
Aws Ayyash <aws.ayyash.1@gmail.com>
2023-06-12 13:40:40 +0300

    fix the conflict

--- app/src/main/java/com/cs/wasselha/Transporter/ProfileTransporterFragment.java
+++ app/src/main/java/com/cs/wasselha/Transporter/ProfileTransporterFragment.java
@@ -10,12 +10,50 @@ import android.view.ViewGroup;
 
 import com.cs.wasselha.R;
 
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+import com.cs.wasselha.R;
+import com.cs.wasselha.RegistrationActivity;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileTransporterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ProfileTransporterFragment extends Fragment {
+
+    private static String apiURL="http://176.119.254.198:8000/wasselha";
+    private ImageView image,settings,claims,cars,favoritos,inbox,logout;
+    private TextView name;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +98,106 @@ public class ProfileTransporterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile_transporter, container, false);
+        setupReference(view);
+        int transporterID=15;
+        setAndGetName(getContext(),transporterID);
+        getVehicleImageURLAndSetImage(getContext(),transporterID);
+
+
+
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //delete info from shared preferences
+                Intent intent = new Intent(getContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_transporter, container, false);
+
     }
+
+
+    private void setupReference(View view) {
+        image = view.findViewById(R.id.signupCustomerBtn);
+        name = view.findViewById(R.id.signupCustomerBtn);
+        settings = view.findViewById(R.id.signupCustomerBtn);
+        claims = view.findViewById(R.id.signupCustomerBtn);
+        cars = view.findViewById(R.id.signupCustomerBtn);
+        favoritos = view.findViewById(R.id.signupCustomerBtn);
+        inbox = view.findViewById(R.id.signupCustomerBtn);
+        logout = view.findViewById(R.id.signupCustomerBtn);
+    }
+    public void setAndGetName(Context context, int transporterID) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = apiURL + "transporters/" + transporterID + "/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String firstName = response.getString("first_name");
+                            String lastName = response.getString("last_name");
+                            String fullName = firstName + " " + lastName;
+
+                            // Assuming that 'name' is a TextView instance you want to set the name to.
+                            name.setText(fullName);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error here
+                error.printStackTrace();
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    public void getVehicleImageURLAndSetImage(Context context, int transporterID) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = apiURL + "vehicles/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            // Assume the vehicles are inside a JSONArray called "vehicles" in the response
+                            JSONArray vehicles = response.getJSONArray("vehicles");
+                            for (int i = 0; i < vehicles.length(); i++) {
+                                JSONObject vehicle = vehicles.getJSONObject(i);
+                                if (vehicle.getInt("transporter") == transporterID) {
+                                    setImage(apiURL+vehicle.getString("vehicle_image"));
+                                    return;
+                                }
+                            }
+                        } catch (Exception e) {
+                            Log.e("profile","Transporter not found");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("profile","failed loading image(Network Issue )");
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+    void setImage(String imageUrl){
+        Glide.with(this)
+                .load(imageUrl)
+                .into(image);
+    }
+
 }
