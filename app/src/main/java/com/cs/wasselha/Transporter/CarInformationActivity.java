bffd1c540a14b5574ce3b1da36f675bc188b257c
ghassan-amreyah <1191704@student.birzeit.edu>
2023-06-16 19:37:15 +0300

    Build part of transporter profile activities

--- app/src/main/java/com/cs/wasselha/Transporter/CarInformationActivity.java
+++ app/src/main/java/com/cs/wasselha/Transporter/CarInformationActivity.java
@@ -2,20 +2,16 @@ package com.cs.wasselha.Transporter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.cs.wasselha.R;
-import com.cs.wasselha.RegistrationActivity;
 
 public class CarInformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_information);
+        setContentView(R.layout.activity_vehicle_information);
         getSupportActionBar().hide();
 
         //Calls
