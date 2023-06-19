5884ea1b804ef757de49f7e4a30a83a9d5d8bed9
ghassan-amreyah <1191704@student.birzeit.edu>
2023-06-19 19:43:38 +0300

    Some fixes in transporter UI

--- app/src/main/java/com/cs/wasselha/Transporter/TransporterReservationDetailsActivity.java
+++ app/src/main/java/com/cs/wasselha/Transporter/TransporterReservationDetailsActivity.java
@@ -50,6 +50,7 @@ public class TransporterReservationDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transporter_reservation_details);
+        getSupportActionBar().hide();
 
         Intent intent = getIntent();
         final int deliveryServiceDetailsId = intent.getIntExtra("deliveryservicedetails", 0);
