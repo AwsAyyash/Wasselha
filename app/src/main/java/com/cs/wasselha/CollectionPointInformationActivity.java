90a537eddea051560355165fc145c053e610915f
ghassan-amreyah <1191704@student.birzeit.edu>
2023-07-11 00:54:30 +0300

    Build the collection point information page and some fixes

---
+++ app/src/main/java/com/cs/wasselha/CollectionPointInformationActivity.java
@@ -0,0 +1,15 @@
+package com.cs.wasselha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectionPointInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_point_information);
+        getSupportActionBar().hide();
+    }
+}
